3ae2cb0a17233d9f5cb92c77960c7272c9c44d30
nexengineer <nexengineer@gmail.com>
2021-02-06 17:09:33 +0530

    Intial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>THis is how thee page will look</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
